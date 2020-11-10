7b05659b396caa78b86c585ce69b24e48bcd2c89
Mark <markmopairat12345@gmail.com>
2020-11-10 17:18:36 +0700

    bike

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -8,12 +8,21 @@ public class ToyData {
 
     public static Bike[] getBikes() {
         Bike[] bikes = new Bike[3];
-        // your code
+        bikes[0]= new Bike("b1",1);
+        bikes[1]= new Bike("b2",2);
+        bikes[2]=new Bike("b2",3);
+        
+        
+       // int i= 0;
+       // for( i=0;i<bikes.length;i++){
+            
+       // }
+        
 
         return bikes;
     }
 
-    public static Person[] getPerson() {
+    /*public static Person[] getPerson() {
         Person[] person = new Person[3];
         // your code
 
@@ -26,13 +35,13 @@ public class ToyData {
 
         return parcels;
     }
-
+*/
     //testing
     public static void main(String[] args) {
         printBike();
-        printPerson();
-        printParcels();
-    }
+       // printPerson();
+       // printParcels();
+    }//
 
     private static void printBike() {
         Bike[] bikes = getBikes();
@@ -41,7 +50,7 @@ public class ToyData {
         }
     }
 
-    private static void printPerson() {
+   /* private static void printPerson() {
         Person[] person = getPerson();
         for (int i = 0; i < person.length; i++) {
             System.out.println(person[i].toString());
@@ -53,6 +62,6 @@ public class ToyData {
         for (int i = 0; i < percels.length; i++) {
             System.out.println(percels[i].toString());
         }
-    }
+    }*/
 
 }
